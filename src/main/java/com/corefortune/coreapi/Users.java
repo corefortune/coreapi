6507a9889c4a77208da3a0962a164000430ef239
nagarajukotha <nagarajkotha24@gmail.com>
2019-11-10 10:33:07 +0530

    complete curd operations

--- src/main/java/com/corefortune/coreapi/Users.java
+++ src/main/java/com/corefortune/coreapi/Users.java
@@ -5,9 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-@Controller
+@RestController
 public class Users {
 
    /*  @Autowired
@@ -17,36 +22,18 @@ public class Users {
     static{
         data.add(new Usercontroller(2,"nagaraju", 22));
         data.add(new Usercontroller(1,"nagaraj",26));
+        
     }
 
-    @GetMapping("/allusers")
+    @GetMapping("/users")
     public List<Usercontroller> usersdata(){
         return data;
 
     }
-
-    public ArrayList<Usercontroller> findall()
-    {
-        return data;
-    }
-
-    public Usercontroller save(Usercontroller u){
-        if(u.getId()==null){
-            int size=data.size();
-            size=size+1;
-            u.setId(size);
-            data.add(u);
-       
-        }
-        return u;
-    }
-
-
-    public Usercontroller findone(int number){
-
+    @GetMapping("/findone/{id}")
+    public Usercontroller usersdata1(@PathVariable int id){
         for(Usercontroller u : data){
-            if(u.getId()==number){
-                System.out.println(number);
+            if(u.getId()==id){
                 return u;
         }
 
@@ -54,8 +41,48 @@ public class Users {
         }
 return null;
     }
-    
 
+
+    @PostMapping("/users")
+    public void adduser(@RequestBody Usercontroller u){
+
+        if(u.getId()==null){
+            int size=data.size();
+            size=size+1;
+            u.setId(size);
+            data.add(u);
+       
+        }
+    }
+
+
+    @PutMapping("/updateuser/{id}")
+
+    public void putuser(@PathVariable int id ,@RequestBody Usercontroller u){
+
+    
+        for (Usercontroller u1 : data) {
+            if(u1.getId()==id){
+            u1.setName(u.getName());
+            u1.setAge(u.getAge());
+            
+            }
+               
+            }
+                
+                   }
+
+@DeleteMapping("deleteuser/{id}")
+
+public void deleteuser(@PathVariable int id ,@RequestBody Usercontroller u){
+    for (Usercontroller u1 : data) {
+        if(u1.getId()==id){
+        data.remove(u1);
+        
+        }
+           
+        }
+    }
 
 
 
